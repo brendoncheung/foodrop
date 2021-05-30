10eaaf78e5705b449495b1a25ab3e88f669ffb32
FTPWTF <paulchen.nz@gmail.com>
2021-05-30 13:22:11 +1200

    emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:io' show Platform;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:foodrop/core/authentication/authentication_service.dart';
 import 'package:foodrop/core/repositories/vendor/vendor_menu_item_repository.dart';
+import 'package:foodrop/core/services/firestore_service.dart';
 import 'package:foodrop/screens/authentication/_sign_up_screen.dart';
 import 'package:foodrop/screens/authentication/authentication_flow_wrapper.dart';
 import 'package:foodrop/screens/client/theme/client_theme_data.dart';
@@ -15,10 +19,16 @@ void main() async {
   // https://stackoverflow.com/questions/63873338/what-does-widgetsflutterbinding-ensureinitialized-d
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  //write something
-  // from paul
-  //do something else
-  // Paul again
+
+  // connect authentication emulator
+  // try {
+  //   String host = Platform.isAndroid ? '10.0.2.2:8080' : ' :8080';
+  //   FirebaseFirestore.instance.settings =
+  //       Settings(host: host, sslEnabled: false, persistenceEnabled: false);
+  //   await FirebaseAuth.instance.useEmulator('http://localhost:9099');
+  // } catch (e) {
+  //   print("unable to connect and use emulator");
+  // }
 
   runApp(FoodropRoot());
 }
